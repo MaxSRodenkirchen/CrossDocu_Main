071fd00ae5a455de31d3b9259333ba5c698e7341
Tomoyasu <70066897+MaxSRodenkirchen@users.noreply.github.com>
2026-08-23 23:44:26 +0200

    clean 2

---
+++ _data/layout.js
@@ -0,0 +1,5 @@
+// Source - https://stackoverflow.com/a/69421709
+// Posted by Jeremy Tarpley, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-08-23, License - CC BY-SA 4.0
+
+module.exports = "layouts/base.html";
---
+++ eleventy.config.js
@@ -0,0 +1,9 @@
+export default function (eleventyConfig) {
+    eleventyConfig.setInputDirectory('src');
+    eleventyConfig.setOutputDirectory('_site');
+}
+
+export const config = {
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+};
